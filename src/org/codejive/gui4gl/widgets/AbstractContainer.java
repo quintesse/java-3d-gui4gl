b8a4152d1d37558eb64098a558f54a658b6c488b
tako <devnull@localhost>
2003-11-14 17:38:42 +0100

    [svn] Added getChild() and findChild() methods. add() now also accepts a name for a child widget.

--- src/org/codejive/gui4gl/widgets/AbstractContainer.java
+++ src/org/codejive/gui4gl/widgets/AbstractContainer.java
@@ -3,16 +3,19 @@
  */
 package org.codejive.gui4gl.widgets;
 
-import java.util.List;
+import java.util.Iterator;
 
 /**
  * @author tako
  */
-public interface AbstractContainer {
-	public abstract void add(Widget _child);
-	public abstract List getChildren();
+public interface AbstractContainer extends AbstractWidget {
+	public abstract void add(AbstractWidget _child);
+	public abstract void add(AbstractWidget _child, String _sName);
+	public abstract Iterator getChildren();
+	public abstract AbstractWidget getChild(String _sName);
+	public abstract AbstractWidget findChild(String _sName);
 	public abstract AbstractWidget getFocusWidget();
-	public abstract void setFocusWidget(Widget _widget);
+	public abstract void setFocusWidget(AbstractWidget _widget);
 	public abstract AbstractWidget getPreviousFocusWidget(AbstractWidget _widget);
 	public abstract AbstractWidget getNextFocusWidget(AbstractWidget _widget);
 }
