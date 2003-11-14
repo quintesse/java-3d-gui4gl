b3f60b887b05c84f1891f91a9e8ea9db7168fe51
tako <devnull@localhost>
2003-11-14 17:37:15 +0100

    [svn] Added code to help widget renderers call their super class renderers.

--- src/org/codejive/gui4gl/themes/RenderHelper.java
+++ src/org/codejive/gui4gl/themes/RenderHelper.java
@@ -5,6 +5,9 @@ package org.codejive.gui4gl.themes;
 
 import java.awt.Rectangle;
 
+import org.codejive.gui4gl.widgets.AbstractWidget;
+import org.codejive.utils4gl.RenderContext;
+
 import net.java.games.jogl.GL;
 
 /**
@@ -22,5 +25,30 @@ public class RenderHelper {
 		_gl.glTexCoord2f(1.0f, 0.0f);
 		_gl.glVertex2f(_rect.x + _rect.width, _rect.y);
 	}
+
+	protected static AbstractWidgetRenderer findFirstSuperClassRenderer(Class _widgetClass) {
+		AbstractWidgetRenderer renderer = null;
+		Class superClass = _widgetClass.getSuperclass();
+		if (superClass != null) {
+			renderer = (AbstractWidgetRenderer)Theme.getValue(superClass, "renderer");
+			if (renderer == null) {
+				renderer = findFirstSuperClassRenderer(superClass);
+			}
+		}
+		return renderer;
+	}
 	
+	public static void initSuperClass(Class _widgetClass, AbstractWidget _widget, RenderContext _context) {
+		AbstractWidgetRenderer renderer = findFirstSuperClassRenderer(_widgetClass);
+		if (renderer != null) {
+			renderer.initRendering(_widget, _context);
+		}
+	}
+
+	public static void renderSuperClass(Class _widgetClass, AbstractWidget _widget, RenderContext _context) {
+		AbstractWidgetRenderer renderer = findFirstSuperClassRenderer(_widgetClass);
+		if (renderer != null) {
+			renderer.render(_widget, _context);
+		}
+	}	
 }
