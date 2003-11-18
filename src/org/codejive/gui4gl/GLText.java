729414aaff71ced4bc7949b6aef032e5ef9345ef
tako <devnull@localhost>
2003-11-18 12:02:13 +0100

    [svn] Improved rendering speed quite a bit.

--- src/org/codejive/gui4gl/GLText.java
+++ src/org/codejive/gui4gl/GLText.java
@@ -13,7 +13,7 @@ import net.java.games.jogl.GL;
 
 /**
  * @author tako
- * @version $Revision: 48 $
+ * @version $Revision: 52 $
  */
 public class GLText {
 	public static final int ALIGN_LEFT = 0;
@@ -39,44 +39,61 @@ public class GLText {
 		int l = _sText.length();
 		// Continue while there are still characters and vertical space left
 		while ((p < l) && ((fYPosDelta + fFontHeight) < fHeight)) {
+			// Skip any leading spaces
 			while ((p < l) && (_sText.charAt(p) == ' ')) {
 				p++;
 			}
 			if (p < l) {
 				StringBuffer s = new StringBuffer();
-				s.append(_sText.charAt(p++));
+				// Add the next word to the string buffer
+				int p2 = _sText.indexOf(' ', p);
+				if (p2 > p) {
+					s.append(_sText.substring(p, p2));
+					p = p2 + 1;
+				} else {
+					s.append(_sText.substring(p));
+					p = l;
+				}
 				float fLength = _font.getTextWidth(_context, s.toString());
-				// Continue adding character while there is still horizontal space left
+				// Continue adding words while there is still horizontal space left
 				while ((p < l) && (fLength < fWidth)) {
-					char c = _sText.charAt(p++);
-					s.append(c);
+					s.append(' ');
+					p2 = _sText.indexOf(' ', p);
+					if (p2 > p) {
+						s.append(_sText.substring(p, p2));
+						p = p2 + 1;
+					} else {
+						s.append(_sText.substring(p));
+						p = l;
+					}
 					fLength = _font.getTextWidth(_context, s.toString());
 				}
-				// Remove last character if we crossed the boundary
+				// Check if the text fragment crossed the boundary
 				if (fLength > fWidth) {
-					s.deleteCharAt(s.length() - 1);
-					p--;
-				}
-				// Determine if this is the last line that will be able to fit
-				boolean bLastLine = (!_bMultiLine || ((fYPosDelta + 2 * fFontHeight + fFontPadding) >= fHeight));
-				if (bLastLine) {
-					if (p < l) {
-						// Let's shorten the fragment even more until ... fits at the end
-						fLength = _font.getTextWidth(_context, s.toString() + "...");
+					// Determine if this is the last line that will be able to fit
+					boolean bLastLine = (!_bMultiLine || ((fYPosDelta + 2 * fFontHeight + fFontPadding) >= fHeight));
+					if (bLastLine) {
+						// Let's shorten the fragment even more until "..." fits at the end
 						while ((s.length() > 0) && (fLength > fWidth)) {
 							s.deleteCharAt(s.length() - 1);
 							fLength = _font.getTextWidth(_context, s.toString() + "...");
 						}
 						s.append("...");
-					}
-				} else {
-					if (inWord(_sText, p)) {
+					} else {
 						// Did we encounter a word boundary somewhere in the fragment?
 						int nLastBreak = s.lastIndexOf(" ");
 						if (nLastBreak >= 0) {
 							// Shorten the string to the last word boundary
-							p -= s.length() - nLastBreak - 1;
+							p -= s.length() - nLastBreak;
 							s.delete(nLastBreak, s.length());
+						} else {
+							// Let's shorten the fragment until it fits again
+							fLength = _font.getTextWidth(_context, s.toString());
+							while ((s.length() > 0) && (fLength > fWidth)) {
+								s.deleteCharAt(s.length() - 1);
+								p--;
+								fLength = _font.getTextWidth(_context, s.toString());
+							}
 						}
 					}
 				}
@@ -109,6 +126,9 @@ public class GLText {
 
 /*
  * $Log$
+ * Revision 1.3  2003/11/18 11:02:13  tako
+ * Improved rendering speed quite a bit.
+ *
  * Revision 1.2  2003/11/17 10:54:49  tako
  * Added CVS macros for revision and log.
  *
