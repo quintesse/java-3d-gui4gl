6ff24f42887b45c41879d80788897157a5cec03c
tako <devnull@localhost>
2003-11-23 03:00:33 +0100

    [svn] Made the propety handling and naming a bit more like the standard.

--- src/org/codejive/gui4gl/themes/blues/TextFieldRenderer.java
+++ src/org/codejive/gui4gl/themes/blues/TextFieldRenderer.java
@@ -10,12 +10,13 @@ import net.java.games.jogl.GL;
 import org.codejive.utils4gl.GLColor;
 import org.codejive.utils4gl.RenderContext;
 
+import org.codejive.gui4gl.fonts.Font;
 import org.codejive.gui4gl.themes.*;
 import org.codejive.gui4gl.widgets.*;
 
 /**
  * @author steven
- * @version $Revision: 120 $
+ * @version $Revision: 125 $
  */
 public class TextFieldRenderer implements WidgetRendererModel {
 	
@@ -30,35 +31,36 @@ public class TextFieldRenderer implements WidgetRendererModel {
 
 		TextField textField = (TextField)_widget;
 		
+		Font textFont;
 		GLColor textFontColor;
 		if(textField.hasFocus()) {
-			textFontColor = textField.getTextFontFocusedColor();
+			textFont = textField.getFocusedTextFont();
+			textFontColor = textField.getFocusedTextFontColor();
 		} else {
+			textFont = textField.getTextFont();
 			textFontColor = textField.getTextFontColor();
 		}
 
 		gl.glDisable(GL.GL_TEXTURE_2D);
-		drawTextWithCursor(_context, _widget.getInnerBounds(), 0, 0, textFontColor, textField);
+		drawTextWithCursor(_context, _widget.getInnerBounds(), 0, 0, textFont, textFontColor, textField);
 		gl.glEnable(GL.GL_TEXTURE_2D);
 	}
 
-	private static void drawTextWithCursor(RenderContext _context, Rectangle _bounds, int _nXPadding, int _nYPadding, GLColor _color, TextField _textField) {
+	private static void drawTextWithCursor(RenderContext _context, Rectangle _bounds, int _nXPadding, int _nYPadding, Font _font, GLColor _color, TextField _textField) {
 		GL gl = _context.getGl();
 
-		
-		float fFontHeight = _textField.getTextFont().getSize(_context);
-		float fFontBaseLine = _textField.getTextFont().getBaseLine(_context);
+		float fFontHeight = _font.getSize(_context);
+		float fFontBaseLine = _font.getBaseLine(_context);
 		float fFontPadding = fFontHeight - fFontBaseLine;
 		float fXPos = _bounds.x + _nXPadding;
 		float fYPos = _bounds.y + _nYPadding + fFontBaseLine + fFontPadding;
 
 		float fCursorXPos = fXPos;
-		float fCursorWidth = 2;
+		float fCursorWidth = 1;
 		
 		float fWidth = _bounds.width - 2 * _nXPadding - fCursorWidth; // make sure cursor always has room.
 		
 		gl.glColor3fv(_color.toArray3f());
-		
 
 		// note that the viewoffset may not be enough to show the cursor, which we adjust for here, since only here we know what fits.
 		int nViewOffset = _textField.getViewOffset();
@@ -76,10 +78,10 @@ public class TextFieldRenderer implements WidgetRendererModel {
 		while((p > 0) && (p > nViewOffset)) {
 			p--;
 			s.insert(0, sText.charAt(p));
-			fLength = _textField.getTextFont().getTextWidth(_context, s.toString());
+			fLength = _font.getTextWidth(_context, s.toString());
 			if(fLength > fWidth) {
 				s.deleteCharAt(0);
-				fLength = _textField.getTextFont().getTextWidth(_context, s.toString());
+				fLength = _font.getTextWidth(_context, s.toString());
 				p++;
 				break;
 			}
@@ -88,8 +90,7 @@ public class TextFieldRenderer implements WidgetRendererModel {
 		_textField.setViewOffset(p);
 		
 		gl.glRasterPos2f(fXPos, fYPos);
-		_textField.getTextFont().renderText(_context, s.toString());
-		
+		_font.renderText(_context, s.toString());
 		
 		s = new StringBuffer(l);
 		float fRightWidth = fWidth - fLength;
@@ -98,7 +99,7 @@ public class TextFieldRenderer implements WidgetRendererModel {
 			p = nCursorPos;
 			while(p < l) {
 				s.append(sText.charAt(p));
-				fLength = _textField.getTextFont().getTextWidth(_context, s.toString());
+				fLength = _font.getTextWidth(_context, s.toString());
 				if(fLength > fRightWidth) {
 					s.deleteCharAt(s.length() - 1);
 					break;
@@ -107,27 +108,30 @@ public class TextFieldRenderer implements WidgetRendererModel {
 				}
 			}
 			gl.glRasterPos2f(fCursorXPos, fYPos);
-			_textField.getTextFont().renderText(_context, s.toString());
+			_font.renderText(_context, s.toString());
 		}
 		
-		
+		// Only show a cursor if we have focus
 		if(_textField.hasFocus()) {
+			// The cursor blinks, so let's check if we should draw it or not
 			int spd = _textField.getCursorBlinkSpeed();
 			if(System.currentTimeMillis() % spd < (spd/2)) {
+				// Draw cursor
 				GLColor c = _textField.getTextCursorColor();
 				gl.glColor4f(c.getRed(), c.getGreen(), c.getBlue(), c.getAlpha());
 				gl.glBegin(GL.GL_QUADS);
-				RenderHelper.drawRectangle(_context.getGl(), (int)fCursorXPos, (int) (fYPos - fFontHeight + fFontPadding), (int)fCursorWidth, (int)(fFontHeight - fFontPadding));
+				RenderHelper.drawRectangle(_context.getGl(), (int)fCursorXPos, (int)(fYPos - fFontHeight), (int)fCursorWidth, (int)(fFontHeight + fFontPadding));
 				gl.glEnd();
 			}
 		}
-		
 	}
-	
 }
 
 /*
  * $Log$
+ * Revision 1.3  2003/11/23 02:00:33  tako
+ * Made the propety handling and naming a bit more like the standard.
+ *
  * Revision 1.2  2003/11/21 10:48:39  steven
  * should not have used padding in combination with innerbounds
  *
