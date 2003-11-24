26946890a8b771714089b0c47db7bbde290c768a
tako <devnull@localhost>
2003-11-24 17:48:28 +0100

    [svn] Added updateRendering().

--- src/org/codejive/gui4gl/themes/WidgetRendererModel.java
+++ src/org/codejive/gui4gl/themes/WidgetRendererModel.java
@@ -8,15 +8,19 @@ import org.codejive.gui4gl.widgets.*;
 
 /**
  * @author tako
- * @version $Revision: 48 $
+ * @version $Revision: 143 $
  */
 public interface WidgetRendererModel {
 	public void initRendering(Widget _widget, RenderContext _context); 
+	public void updateRendering(Widget _widget, RenderContext _context); 
 	public void render(Widget _widget, RenderContext _context);
 }
 
 /*
  * $Log$
+ * Revision 1.4  2003/11/24 16:48:28  tako
+ * Added updateRendering().
+ *
  * Revision 1.3  2003/11/17 10:54:49  tako
  * Added CVS macros for revision and log.
  *
