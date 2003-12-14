d3cf79eb733b3bfafb704c6e7313d667a7478c5b
tako <devnull@localhost>
2003-12-14 03:36:26 +0100

    [svn] CompoundWidget added that has almost all the functionality of Container but with all protected methods meant for complex widgets. Container has become little more than a public version of CompoundWidget.

---
+++ src/org/codejive/gui4gl/widgets/CompoundWidget.java
@@ -0,0 +1,236 @@
+/*
+ * Created on Dec 14, 2003
+ */
+package org.codejive.gui4gl.widgets;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
+
+import org.codejive.utils4gl.RenderContext;
+
+/**
+ * @author tako
+ * @version $Revision: 201 $
+ */
+public class CompoundWidget extends Widget {
+	protected LinkedList m_children;
+	protected Map m_childNames;
+	protected Widget m_focusWidget;
+
+	private static int m_nWidgetNr = 0;
+
+	public CompoundWidget() {
+		m_children = new LinkedList();
+		m_childNames = new HashMap();
+		m_focusWidget = null;
+		setFocusable(true);
+	}
+
+	protected void add(Widget _child) {
+		add(_child, "widget" + m_nWidgetNr++);
+	}
+
+	protected void add(Widget _child, String _sName) {
+		m_children.add(_child);
+		m_childNames.put(_sName, _child);
+		_child.setParent(this);
+		_child.setName(_sName);
+	}
+
+	protected void remove(Widget _child) {
+		m_children.remove(_child);
+		m_childNames.remove(_child);
+		_child.setParent(null);
+	}
+	
+	protected void remove(String _sChildName) {
+		Widget child = getChild(_sChildName);
+		remove(child);
+	}
+	
+	protected Widget getChild(String _sName) {
+		return (Widget)m_childNames.get(_sName);
+	}
+
+	protected Iterator getChildren() {
+		List x = new LinkedList(m_children);
+		return x.iterator();
+	}
+
+	protected Widget findChild(String _sName) {
+		Widget found = null;
+		// First see if the current container contains a widget with the given name
+		Widget child = getChild(_sName);
+		if (child == null) {
+			Iterator i = getChildren();
+			while ((found == null) && i.hasNext()) {
+				child = (Widget)i.next();
+				if (child instanceof Container) {
+					found = ((Container)child).findChild(_sName);
+				}
+			}
+		} else {
+			found = child;
+		}
+		// Now see if any of our children is a container and has 
+		return found;
+	}
+
+	protected Widget getWidgetUnderPoint(int _nXPos, int _nYPos) {
+		Widget result = null;
+		if (isVisible()) {
+			Iterator i = getChildren();
+			while ((result == null) && i.hasNext()) {
+				Widget w = (Widget)i.next();
+				result = w.getWidgetUnderPoint(_nXPos, _nYPos);
+			}
+			if (result == null) {
+				result = super.getWidgetUnderPoint(_nXPos, _nYPos);
+			}
+		}
+		return result;
+	}
+
+	protected void moveChildToFront(Widget _child) {
+		m_children.remove(_child);
+		m_children.addLast(_child);
+	}
+
+	protected void moveChildToBack(Widget _child) {
+		m_children.remove(_child);
+		m_children.addFirst(_child);
+	}
+
+	protected Widget getFocusWidget() {
+		return m_focusWidget;
+	}
+	
+	protected void setFocusWidget(Widget _widget) {
+		m_focusWidget = _widget;
+		if (getParent() != null) {
+			getParent().setFocusWidget(_widget);
+		}
+	}
+
+	protected Widget getPreviousFocusWidget(Widget _widget) {
+		Widget prevWidget = null;
+		int p;
+		if (_widget != null) {
+			p = m_children.indexOf(_widget);
+		} else {
+			p = m_children.size();
+		}
+		if (p >= 0) {
+			ListIterator i = m_children.listIterator(p);
+			while (i.hasPrevious() && (prevWidget == null)) {
+				Widget w = (Widget)i.previous();
+				if (w.isFocusable()) {
+					prevWidget = w;
+				} else if (w instanceof Container) {
+					Container cw = (Container)w;
+					w = cw.getPreviousFocusWidget(null);
+					if (w != null) {
+						prevWidget = w;
+					}
+				}
+			}
+			if (prevWidget == null) {
+				// No focusable widget was found, let's ask our parent
+				prevWidget = getParent().getPreviousFocusWidget(this);
+			}
+			if ((prevWidget == null) && (_widget != null)) {
+				// No focusable widget was found, let's try again from the end of the list
+				prevWidget = getPreviousFocusWidget(null);
+			}
+		}
+		if (prevWidget == null) {
+			// No previous widget could be determined, return current value
+			prevWidget = m_focusWidget;
+		}
+		return prevWidget;
+	}
+
+	protected Widget getNextFocusWidget(Widget _widget) {
+		Widget nextWidget = null;
+		int p;
+		if (_widget != null) {
+			p = m_children.indexOf(_widget);
+			if (p >= 0) {
+				p++;
+			}
+		} else {
+			p = 0;
+		}
+		if (p >= 0) {
+			ListIterator i = m_children.listIterator(p);
+			while (i.hasNext() && (nextWidget == null)) {
+				Widget w = (Widget)i.next();
+				if (w.isFocusable()) {
+					nextWidget = w;
+				} else if (w instanceof Container) {
+					Container cw = (Container)w;
+					w = cw.getNextFocusWidget(null);
+					if (w != null) {
+						nextWidget = w;
+					}
+				}
+			}
+			if (nextWidget == null) {
+				// No focusable widget was found, let's ask our parent
+				nextWidget = getParent().getNextFocusWidget(this);
+			}
+			if ((nextWidget == null) && (_widget != null)) {
+				// No focusable widget was found, let's try again from the beginning of the list
+				nextWidget = getNextFocusWidget(null);
+			}
+		}
+		if (nextWidget == null) {
+			// No next widget could be determined, return current value
+			nextWidget = m_focusWidget;
+		}
+		return nextWidget;
+	}
+
+	public void initRendering(RenderContext _context) {
+		super.initRendering(_context);
+		if (isVisible()) {
+			initChildren(_context);
+		}
+	}
+
+	protected void initChildren(RenderContext _context) {
+		Iterator i = getChildren();
+		while (i.hasNext()) {
+			Widget w = (Widget)i.next();
+			w.initRendering(_context);
+		}
+	}
+
+	public void render(RenderContext _context) {
+		super.render(_context);
+		if (isVisible()) {
+			renderChildren(_context);
+		}
+	}
+
+	protected void renderChildren(RenderContext _context) {
+		Iterator i = getChildren();
+		while (i.hasNext()) {
+			Widget w = (Widget)i.next();
+			w.render(_context);
+		}
+	}
+}
+
+/*
+ * $Log$
+ * Revision 1.1  2003/12/14 02:36:26  tako
+ * CompoundWidget added that has almost all the functionality of Container
+ * but with all protected methods meant for complex widgets. Container has
+ * become little more than a public version of CompoundWidget.
+ *
+ */
--- src/org/codejive/gui4gl/widgets/Container.java
+++ src/org/codejive/gui4gl/widgets/Container.java
@@ -22,70 +22,34 @@
 package org.codejive.gui4gl.widgets;
 
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.List;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Map;
 
 import org.codejive.gui4gl.events.GuiKeyEvent;
-import org.codejive.utils4gl.RenderContext;
 
 /**
  * @author tako
- * @version $Revision: 158 $
+ * @version $Revision: 201 $
  */
-public class Container extends Widget {
-	private LinkedList m_children;
-	private Map m_childNames;
-	private Widget m_focusWidget;
-
-	private static int m_nWidgetNr = 0;
-	
-	public Container() {
-		m_children = new LinkedList();
-		m_childNames = new HashMap();
-		m_focusWidget = null;
-		setFocusable(true);
-	}
+public class Container extends CompoundWidget {
 	
 	public void add(Widget _child) {
-		add(_child, "widget" + m_nWidgetNr++);
+		super.add(_child);
 	}
-	
+
 	public void add(Widget _child, String _sName) {
-		m_children.add(_child);
-		m_childNames.put(_sName, _child);
-		_child.setParent(this);
+		super.add(_child, _sName);
 	}
-	
-	public Widget getChild(String _sName) {
+
+	protected Widget getChild(String _sName) {
 		return (Widget)m_childNames.get(_sName);
 	}
 
 	public Iterator getChildren() {
-		List x = new LinkedList(m_children);
-		return x.iterator();
+		return super.getChildren();
 	}
-	
+
 	public Widget findChild(String _sName) {
-		Widget found = null;
-		// First see if the current container contains a widget with the given name
-		Widget child = getChild(_sName);
-		if (child == null) {
-			Iterator i = getChildren();
-			while ((found == null) && i.hasNext()) {
-				child = (Widget)i.next();
-				if (child instanceof Container) {
-					found = ((Container)child).findChild(_sName);
-				}
-			}
-		} else {
-			found = child;
-		}
-		// Now see if any of our children is a container and has 
-		return found;
+		return super.findChild(_sName);
 	}
 
 	public boolean hasFocus() {
@@ -98,117 +62,6 @@ public class Container extends Widget {
 	
 	public Widget getFocusWidget() {
 		return m_focusWidget;
-	}
-	
-	public void setFocusWidget(Widget _widget) {
-		m_focusWidget = _widget;
-		if (getParent() != null) {
-			getParent().setFocusWidget(_widget);
-		}
-	}
-
-	public Widget getPreviousFocusWidget(Widget _widget) {
-		Widget prevWidget = null;
-		int p;
-		if (_widget != null) {
-			p = m_children.indexOf(_widget);
-		} else {
-			p = m_children.size();
-		}
-		if (p >= 0) {
-			ListIterator i = m_children.listIterator(p);
-			while (i.hasPrevious() && (prevWidget == null)) {
-				Widget w = (Widget)i.previous();
-				if (w.isFocusable()) {
-					prevWidget = w;
-				} else if (w instanceof Container) {
-					Container cw = (Container)w;
-					w = cw.getPreviousFocusWidget(null);
-					if (w != null) {
-						prevWidget = w;
-					}
-				}
-			}
-			if (prevWidget == null) {
-				// No focusable widget was found, let's ask our parent
-				prevWidget = getParent().getPreviousFocusWidget(this);
-			}
-			if ((prevWidget == null) && (_widget != null)) {
-				// No focusable widget was found, let's try again from the end of the list
-				prevWidget = getPreviousFocusWidget(null);
-			}
-		}
-		if (prevWidget == null) {
-			// No previous widget could be determined, return current value
-			prevWidget = m_focusWidget;
-		}
-		return prevWidget;
-	}
-
-	public Widget getNextFocusWidget(Widget _widget) {
-		Widget nextWidget = null;
-		int p;
-		if (_widget != null) {
-			p = m_children.indexOf(_widget);
-			if (p >= 0) {
-				p++;
-			}
-		} else {
-			p = 0;
-		}
-		if (p >= 0) {
-			ListIterator i = m_children.listIterator(p);
-			while (i.hasNext() && (nextWidget == null)) {
-				Widget w = (Widget)i.next();
-				if (w.isFocusable()) {
-					nextWidget = w;
-				} else if (w instanceof Container) {
-					Container cw = (Container)w;
-					w = cw.getNextFocusWidget(null);
-					if (w != null) {
-						nextWidget = w;
-					}
-				}
-			}
-			if (nextWidget == null) {
-				// No focusable widget was found, let's ask our parent
-				nextWidget = getParent().getNextFocusWidget(this);
-			}
-			if ((nextWidget == null) && (_widget != null)) {
-				// No focusable widget was found, let's try again from the beginning of the list
-				nextWidget = getNextFocusWidget(null);
-			}
-		}
-		if (nextWidget == null) {
-			// No next widget could be determined, return current value
-			nextWidget = m_focusWidget;
-		}
-		return nextWidget;
-	}
-	
-	protected Widget getWidgetUnderPoint(int _nXPos, int _nYPos) {
-		Widget result = null;
-		if (isVisible()) {
-			Iterator i = getChildren();
-			while ((result == null) && i.hasNext()) {
-				Widget w = (Widget)i.next();
-				result = w.getWidgetUnderPoint(_nXPos, _nYPos);
-			}
-			if (result == null) {
-				result = super.getWidgetUnderPoint(_nXPos, _nYPos);
-			}
-		}
-		return result;
-	}
-	
-	protected void moveChildToFront(Widget _child) {
-		m_children.remove(_child);
-		m_children.addLast(_child);
-	}
-	
-	protected void moveChildToBack(Widget _child) {
-		m_children.remove(_child);
-		m_children.addFirst(_child);
 	}
 	
 	protected void processKeyPressedEvent(GuiKeyEvent _event) {
@@ -235,46 +88,15 @@ public class Container extends Widget {
 			super.processKeyPressedEvent(_event);
 		}
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.codejive.world3d.Renderable#initRendering(org.codejive.world3d.RenderContext)
-	 */
-	public void initRendering(RenderContext _context) {
-		super.initRendering(_context);
-		if (isVisible()) {
-			initChildren(_context);
-		}
-	}
-	
-	protected void initChildren(RenderContext _context) {
-		Iterator i = getChildren();
-		while (i.hasNext()) {
-			Widget w = (Widget)i.next();
-			w.initRendering(_context);
-		}
-	}
-
-	/* (non-Javadoc)
-	 * @see org.codejive.world3d.Renderable#render(org.codejive.world3d.RenderContext)
-	 */
-	public void render(RenderContext _context) {
-		super.render(_context);
-		if (isVisible()) {
-			renderChildren(_context);
-		}
-	}
-	
-	protected void renderChildren(RenderContext _context) {
-		Iterator i = getChildren();
-		while (i.hasNext()) {
-			Widget w = (Widget)i.next();
-			w.render(_context);
-		}
-	}
 }
 
 /*
  * $Log$
+ * Revision 1.13  2003/12/14 02:36:26  tako
+ * CompoundWidget added that has almost all the functionality of Container
+ * but with all protected methods meant for complex widgets. Container has
+ * become little more than a public version of CompoundWidget.
+ *
  * Revision 1.12  2003/11/25 16:28:00  tako
  * All code is now subject to the Lesser GPL.
  *
